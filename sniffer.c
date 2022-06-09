a6c112302cf17f3276a02d3419a04609b475c963
adiy55 <adiyafe55@gmail.com>
2022-06-09 19:17:00 +0300

    Minor edits

--- sniffer.c
+++ sniffer.c
@@ -1,52 +1,44 @@
-//
-// Created by Eitan Kats on 08/06/2022.
-//
 #include <pcap.h>
 #include <stdio.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <linux/if_packet.h>
 #include <net/ethernet.h>
-#include <stdio.h>
-
 
 struct ethheader {
-    u_char  ether_dhost[ETHER_ADDR_LEN]; /* destination host address */
-    u_char  ether_shost[ETHER_ADDR_LEN]; /* source host address */
+    u_char ether_dhost[ETHER_ADDR_LEN]; /* destination host address */
+    u_char ether_shost[ETHER_ADDR_LEN]; /* source host address */
     u_short ether_type;                  /* IP? ARP? RARP? etc */
 };
 
-
 /* IP Header */
 struct ipheader {
-    unsigned char      iph_ihl:4, //IP header length
-    iph_ver:4; //IP version
-    unsigned char      iph_tos; //Type of service
+    unsigned char iph_ihl: 4, //IP header length
+    iph_ver: 4; //IP version
+    unsigned char iph_tos; //Type of service
     unsigned short int iph_len; //IP Packet length (data + header)
     unsigned short int iph_ident; //Identification
-    unsigned short int iph_flag:3, //Fragmentation flags
-    iph_offset:13; //Flags offset
-    unsigned char      iph_ttl; //Time to Live
-    unsigned char      iph_protocol; //Protocol type
+    unsigned short int iph_flag: 3, //Fragmentation flags
+    iph_offset: 13; //Flags offset
+    unsigned char iph_ttl; //Time to Live
+    unsigned char iph_protocol; //Protocol type
     unsigned short int iph_chksum; //IP datagram checksum
-    struct  in_addr    iph_sourceip; //Source IP address
-    struct  in_addr    iph_destip;   //Destination IP address
+    struct in_addr iph_sourceip; //Source IP address
+    struct in_addr iph_destip;   //Destination IP address
 };
 
-void got_packet(u_char *args, const struct pcap_pkthdr *header,
-                const u_char *packet)
-{
-    struct ethheader *eth = (struct ethheader *)packet;
+void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet) {
+    struct ethheader *eth = (struct ethheader *) packet;
 
     if (ntohs(eth->ether_type) == 0x0800) { // 0x0800 is IP type
-        struct ipheader * ip = (struct ipheader *)
+        struct ipheader *ip = (struct ipheader *)
                 (packet + sizeof(struct ethheader));
 
         printf("       From: %s\n", inet_ntoa(ip->iph_sourceip));
         printf("         To: %s\n", inet_ntoa(ip->iph_destip));
 
         /* determine protocol */
-        switch(ip->iph_protocol) {
+        switch (ip->iph_protocol) {
             case IPPROTO_TCP:
                 printf("   Protocol: TCP\n");
                 return;
@@ -83,9 +75,3 @@ int main() {
     pcap_close(handle);   //Close the handle
     return 0;
 }
-
-
-/**********************************************
- * Code on Page 213 (Section 12.2.4)
- **********************************************/
-
